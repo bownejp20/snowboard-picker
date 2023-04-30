e963165fb0a95d21c87ac6c6b44b61da980674f5
bownejp20 <jbowne24@gmail.com>
2023-04-29 20:40:14 -0400

    finished css

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://jbowne24:paige24jb@cluster0.tkio3fd.mongodb.net/snowboards?retryWrites=true&w=majority", 
+    'dbName': 'snowboards'
+};
